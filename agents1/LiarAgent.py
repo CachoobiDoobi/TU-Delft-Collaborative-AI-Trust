ea8954c8c8dec81fe7ee5c02469db3355ad40eb1
Iulia <I.C.Angelescu@student.tudelft.nl>
2022-03-30 23:31:14 +0200

    Add new trust

--- agents1/LiarAgent.py
+++ agents1/LiarAgent.py
@@ -46,6 +46,7 @@ class LiarAgent(BW4TBrain):
         self._door = None
         self._trust = {}
         self._arrayWorld = None
+        self.receivedMessagesIndex = 0
 
     def initialize(self):
         super().initialize()
@@ -76,13 +77,15 @@ class LiarAgent(BW4TBrain):
         self.write_beliefs()
         # self._sendMessage(Util.reputationMessage(self._trust, self._teamMembers), agent_name)
 
-        # Update information based on gathered information
-        if self._arrayWorld is None:
-            self._arrayWorld = np.empty(state['World']['grid_shape'], dtype=list)
+        self._prepareArrayWorld(state)
+        self.updateGoalBlocks(state)
 
-        Util.update_info_general(self._arrayWorld, receivedMessages, self._teamMembers,
-                                 self.foundGoalBlockUpdate, self.foundBlockUpdate, self.pickUpBlockUpdate,
-                                 self.dropBlockUpdate, self.dropGoalBlockUpdate, self.updateRep)
+        # Update information based on gathered information
+        self._updateWorld(state)
+        self._sendMessage(Util.reputationMessage(self._trust, self._teamMembers), agent_name)
+        Util.update_info_general(self._arrayWorld, receivedMessages, self._teamMembers, self.foundGoalBlockUpdate,
+                                 self.foundBlockUpdate, self.pickUpBlockUpdate, self.pickUpBlockSimpleUpdate, self.dropBlockUpdate,
+                                 self.dropGoalBlockUpdate, self.updateRep, agent_name)
 
         # Get agent location & close objects
         agentLocation = state[self.agent_id]['location']
@@ -95,7 +98,7 @@ class LiarAgent(BW4TBrain):
                            if 'CollectableBlock' in obj['class_inheritance']]
 
         # Update trust beliefs for team members
-        self._trustBlief(state, closeBlocks)
+        # self._trustBlief(state, closeBlocks)
 
         # Update arrayWorld
         for obj in closeObjects:
@@ -352,23 +355,199 @@ class LiarAgent(BW4TBrain):
         '''
         Process incoming messages and create a dictionary with received messages from each team member.
         '''
+        reduced_received_messages = self.received_messages[self.receivedMessagesIndex:]
         receivedMessages = {}
         for member in teamMembers:
             receivedMessages[member] = []
-        for mssg in self.received_messages:
+        for mssg in reduced_received_messages:
             for member in teamMembers:
                 if mssg.from_id == member:
                     receivedMessages[member].append(mssg.content)
+        self.receivedMessagesIndex = len(self.received_messages)
         return receivedMessages
+
+    #######################
+    #    PREPARE WORLD    #
+    #######################
+
+    def _prepareArrayWorld(self, state):
+        worldShape = state['World']['grid_shape']
+        if self._arrayWorld is None:
+            self._arrayWorld = np.empty(worldShape, dtype=list)
+            for x in range(worldShape[0]):
+                for y in range(worldShape[1]):
+                    self._arrayWorld[x, y] = []
+
+    def _updateWorld(self, state):
+        agentLocation = state[self.agent_id]['location']
+        closeObjects = state.get_objects_in_area((agentLocation[0] - 1, agentLocation[1] - 1),
+                                                 bottom_right=(agentLocation[0] + 1, agentLocation[1] + 1))
+        # Filter out only blocks
+        closeBlocks = None
+        if closeObjects is not None:
+            closeBlocks = [obj for obj in closeObjects
+                           if 'CollectableBlock' in obj['class_inheritance']]
+
+        # Update trust beliefs for team members
+        self._trustBlief2(state, closeBlocks)
+
+        for member in self._teamMembers:
+            avg = 0
+            for key in self._trust[member].keys():
+                if key in ["pick-up", "drop-off", "found"]:
+                    avg += self._trust[member][key] / 3.0
+            self._trust[member]['average'] = avg
 
     ###############
     #    TRUST    #
     ###############
 
+    def _trustBlief2(self, state, close_objects):
+        agentLocation = state[self.agent_id]['location']
+        (x, y) = agentLocation
+        messages = self._arrayWorld[x][y]
+        self._arrayWorld[x][y] = []
+        if len(messages) > 0: #there is some sort of block interaction!
+            realBlock = self.getObjectAtLocation(close_objects, (x, y))
+            if realBlock == "MultipleObj":
+                return
+            if realBlock is None: #no actual block there so interaction must end with pickup to be valid!
+                self.checkPickUpInteraction(messages)
+            else: #block is there so interaction must end with found or drop-off to be valid!
+                self.checkFoundInteraction(messages, realBlock)
+
+    def checkPickUpInteraction(self,
+                               interactions):  # assume interactions are for the same type of block(same visualization)
+
+        actionFreq = {
+            "drop-off": 0,
+            "found": 0,
+            "pick-up": 0
+        }
+        properActionOrder = True
+        lastActionNotCorrect = False
+        members = []
+        for i in range(len(interactions)):
+            inter = interactions[i]
+            action = inter['action']
+            members.append((inter['memberName'], action))
+            # inter['block']
+            actionFreq[action] += 1
+            if i == len(interactions) - 1 and action != 'pick-up':
+                lastActionNotCorrect = True  # wrong! decrease trust
+            if action == 'drop-off':
+                if i == len(interactions) - 1:
+                    break
+                if interactions[i + 1]['action'] == 'found':
+                    continue  # good! can be continued!
+                else:
+                    properActionOrder = False
+            elif action == 'found':
+                if i == len(interactions) - 1:
+                    break
+                if interactions[i + 1]['action'] == 'found' or interactions[i + 1]['action'] == 'pick-up':
+                    continue  # good! can be continued!
+                else:
+                    properActionOrder = False
+            elif action == 'pick-up':
+                if i == len(interactions) - 1:  # correct case!
+                    continue  # increase trust
+                elif interactions[i + 1]['action'] == 'drop-off':
+                    continue  # good! can be continued!
+                else:
+                    properActionOrder = False
+        if properActionOrder and not lastActionNotCorrect:
+            if actionFreq["drop-off"] + actionFreq['found'] < 1 and actionFreq['pick-up'] == 1:
+                self.increaseDecreaseTrust(members, False)  # decrease (cannot pickup block that has never been found!!)
+            self.increaseDecreaseTrust(members, True)  # increase trust of all agents
+        elif properActionOrder and lastActionNotCorrect:
+            if actionFreq["drop-off"] + actionFreq['found'] > 1:
+                return  # keep the same trust
+            else:
+                self.increaseDecreaseTrust(members, False)  # decrease trust
+        else:
+            self.increaseDecreaseTrust(members, False)  # decrease trust
+
+    def increaseDecreaseTrust(self, members, isIncrease, block=None):
+
+        val = -0.1
+        if isIncrease:
+            val = 0.1
+        for member in members:
+            if block is not None:
+                val = self.check_same_visualizations(block['visualization'], member[2])
+            self._trust[member[0]][member[1]] = min(max(round(self._trust[member[0]][member[1]] + val, 3), 0), 1)
+            self._trust[member[0]]['verified'] += 1
+
+    def checkFoundInteraction(self, interactions, realBlock):
+        actionFreq = {
+            "drop-off": 0,
+            "found": 0,
+            "pick-up": 0
+        }
+        properActionOrder = True
+        lastActionNotCorrect = False
+        members = []
+        for i in range(len(interactions)):
+            inter = interactions[i]
+            action = inter['action']
+            members.append((inter['memberName'], action, inter['block']))
+            # inter['block']
+            actionFreq[action] += 1
+            if i == len(interactions) - 1:
+                if action == 'pick-up':
+                    lastActionNotCorrect = True  # wrong! decrease trust
+                break
+            if action == 'drop-off':
+                if interactions[i + 1]['action'] == 'found':
+                    continue  # good! can be continued!
+                else:
+                    properActionOrder = False
+            elif action == 'found':
+                if interactions[i + 1]['action'] == 'found' or interactions[i + 1]['action'] == 'pick-up':
+                    continue  # good! can be continued!
+                else:
+                    properActionOrder = False
+            elif action == 'pick-up':
+                if interactions[i + 1]['action'] == 'drop-off':
+                    continue  # good! can be continued!
+                else:
+                    properActionOrder = False
+
+        if properActionOrder and not lastActionNotCorrect:
+            self.increaseDecreaseTrust(members, True, realBlock)  # increase trust of all agents
+        else:
+            self.increaseDecreaseTrust(members, False)  # decrease trust
+
+    def getObjectAtLocation(self, close_objects, location):
+        closeBlocks = None
+        if close_objects is not None:
+            closeBlocks = [obj for obj in close_objects
+                           if location == obj['location']]
+        if len(closeBlocks) == 0:
+            return None
+        if len(closeBlocks) != 1:
+            return "MultipleObj"
+        return closeBlocks[0]
+    def check_same_visualizations(self, vis1, vis2):
+        size = 0
+        shape = 0
+        colour = 0
+        if "shape" in vis1 and "shape" in vis2:
+            shape = 0.05 if vis1['shape'] == vis2['shape'] else -0.05
+
+        if "colour" in vis1 and "colour" in vis2:
+            colour = 0.05 if vis1['colour'] == vis2['colour'] else -0.05
+
+        return size + shape + colour
+
+    ##########################
+    #    READ WRITE TRUST    #
+    ##########################
+
     def read_trust(self):
         # agentname_trust.csv
         file_name = self.agent_id + '_trust.csv'
-        # fprint(file_name)
         if os.path.exists(file_name):
             with open(file_name, newline='') as file:
                 reader = csv.reader(file, delimiter=',')
@@ -377,6 +556,7 @@ class LiarAgent(BW4TBrain):
                         self._trust[row[0]] = {"pick-up": float(row[1]), "drop-off": float(row[2]),
                                                "found": float(row[3]),
                                                "average": float(row[4]),
+
                                                "rep": float(row[5]), "verified": float(row[6])}
         else:
             f = open(file_name, 'x')
@@ -385,7 +565,7 @@ class LiarAgent(BW4TBrain):
     def initialize_trust(self):
         team = self._teamMembers
         for member in team:
-            self._trust[member] = {"name": member, "pick-up": 0.5, "drop-off": 0.5, "found": 0.5, "average": 0.5,
+            self._trust[member] = {"pick-up": 0.5, "drop-off": 0.5, "found": 0.5, "average": 0.5,
                                    "rep": 0.5, "verified": 0}
 
     def write_beliefs(self):
@@ -398,75 +578,6 @@ class LiarAgent(BW4TBrain):
                 row = self._trust[name]
                 row['name'] = name
                 writer.writerow(row)
-
-    def _trustBlief(self, state, close_objects):
-        # print(self._arrayWorld)
-        # print("l: ", self._trust)
-        if close_objects is not None:
-            for o in close_objects:
-                loc = o['location']
-                messages = self._arrayWorld[loc[0], loc[1]]
-                # If we find messages for the location of the object
-                if messages is not None and len(messages) > 0:
-                    member = messages[-1]['memberName']
-                    # If last message is 'pick-up' substract from trust
-                    if messages[-1]['action'] == "pick-up":
-                        self._trust[member]['pick-up'] = max(round(self._trust[member]['pick-up'] - 0.1, 3), 0)
-                    # If last message is 'found' or 'drop-of' add to trust
-                    if messages[-1]['action'] == "found" or messages[-1]['action'] == "drop-off":
-                        val = self.check_same_visualizations(o['visualization'], messages[-1]['block'])
-                        self._trust[member]['found'] = min(round(self._trust[member]['found'] + val, 3), 1)
-                    if len(messages) > 1:
-                        i = len(messages) - 2
-                        while i >= 0:
-                            member = messages[i]['memberName']
-                            if messages[-1]['action'] == "drop-off":
-                                self._trust[member]['drop-off'] = min(round(self._trust[member]['drop-off'] + 0.1, 3),
-                                                                      1)
-                                break
-                            if not messages[-1]['action'] == "found":
-                                break
-
-                            val = self.check_same_visualizations(o['visualization'], messages[-1]['block'])
-                            self._trust[member]['found'] = min(round(self._trust[member]['found'] + val, 3), 1)
-
-                            i -= 1
-
-        agentLocation = state[self.agent_id]['location']
-        for x in range(agentLocation[0] - 2, agentLocation[0] + 2):
-            for y in range(agentLocation[1] - 2, agentLocation[1] + 2):
-                messages = self._arrayWorld[x][y]
-                if messages is not None and len(messages) > 0:
-                    member = messages[-1]['memberName']
-                    if isinstance(messages, list) and messages[-1]['action'] == "found" or messages[-1][
-                        'action'] == "drop-off":
-                        if close_objects is None:
-                            self._trust[member][messages[-1]['action']] = max(
-                                round(self._trust[member][messages[-1]['action']] - 0.1, 3), 0)
-                        else:
-                            found = False
-                            for o in close_objects:
-                                if o['location'] == (x, y):
-                                    found = True
-                            if found is False:
-                                self._trust[member][messages[-1]['action']] = max(
-                                    round(self._trust[member][messages[-1]['action']] - 0.1, 3), 0)
-
-    def check_same_visualizations(self, vis1, vis2):
-        size = 0
-        shape = 0
-        colour = 0
-
-        if "size" in vis1 and "size" in vis2:
-            size = 0.033 if vis1['size'] == vis2['size'] else -0.033
-
-        if "shape" in vis1 and "shape" in vis2:
-            shape = 0.033 if vis1['shape'] == vis2['shape'] else -0.033
-
-        if "colour" in vis1 and "colour" in vis2:
-            colour = 0.033 if vis1['colour'] == vis2['colour'] else -0.033
-
-        return size + shape + colour
 
     ############################
     #     CHECK GOAL BLOCK     #
@@ -524,5 +635,10 @@ class LiarAgent(BW4TBrain):
     def updateGoalBlocks(self, state):
         return
 
-    def updateRep(self, avg):
+    def pickUpBlockSimpleUpdate(self, block, member):
         return
+
+    def updateRep(self, avg_reps):
+        for member in self._teamMembers:
+            self._trust[member]['rep'] = avg_reps[member] / len(self._teamMembers)
+
