9d6ae2a1f47f1feaa4a852a1f2a9f8eea8f10fb2
radug <rgaghi@student.tudelft.nl>
2022-03-30 21:36:12 +0200

    i wanna keep main

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from agents1.StrongAgent import StrongAgent
-from agents1.LiarAgent import LiarAgent
 from agents1.BlindAgent import BlindAgent
+from agents1.LazyAgent import LazyAgent
+from agents1.LiarAgent import LiarAgent
 from bw4t.BW4TWorld import BW4TWorld
 from bw4t.statistics import Statistics
-from agents1.BW4TBaselineAgent import BaseLineAgent
-from agents1.BW4THuman import Human
-from agents1.LazyAgent import LazyAgent
 
 """
 This runs a single session. You have to log in on localhost:3000 and 
@@ -14,12 +11,17 @@ press the start button in god mode to start the session.
 
 if __name__ == "__main__":
     agents = [
-        {'name':'liar', 'botclass':LiarAgent, 'settings':{}},
+       # {'name':'liar1', 'botclass':LiarAgent, 'settings':{}},
+        #{'name': 'liar2', 'botclass': LiarAgent, 'settings': {}},
+        #{'name': 'liar3', 'botclass': LiarAgent, 'settings': {}},
         #{'name':'blind', 'botclass':BlindAgent, 'settings':{}},
-        #{'name':'lazy3', 'botclass':LazyAgent, 'settings':{}},
-        #{'name': 'lazy1', 'botclass': LazyAgent, 'settings': {}},
+        {'name':'lazy3', 'botclass':LazyAgent, 'settings':{}},
+        {'name': 'lazy1', 'botclass': LazyAgent, 'settings': {}},
         {'name': 'lazy2', 'botclass': LazyAgent, 'settings': {}},
-        {'name': 'strong', 'botclass': StrongAgent, 'settings': {}},
+        {'name': 'lazy4', 'botclass': LazyAgent, 'settings': {}},
+        {'name': 'lazy5', 'botclass': LazyAgent, 'settings': {}},
+       #{'name': 'strong', 'botclass': StrongAgent, 'settings': {}},
+        #{'name': 'humna', 'botclass' : Human, 'settings':{}},
         ]
 
     print("Started world...")
