e58c52d05e87fe8ad4822aa2354e74ab9dd1ed5d
Ana Vasilcoiu <a.m.vasilcoiu@student.tudelft.nl>
2022-03-30 19:57:09 +0200

    fix lazy in blind branch

--- agents1/LazyAgent.py
+++ agents1/LazyAgent.py
@@ -706,3 +706,4 @@ class LazyAgent(BW4TBrain):
     def updateRep(self, avg_reps):
         for member in self._teamMembers:
             self._trust[member]['rep'] = avg_reps[member] / len(self._teamMembers)
+
